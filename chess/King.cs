c12a0163a5fbed2eb469dfb613a364631a61ff26
JuliusM666 <119590444+JuliusM666@users.noreply.github.com>
2024-04-06 14:56:29 +0300

    add files

---
+++ chess/King.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chess
+{
+    class King : Figure
+    {
+        private bool FirstTurn;
+        public King(Char color,bool FirstTurn)
+        {
+            this.FirstTurn = FirstTurn;
+            this.Color = color;
+        }
+        public override void Moves(int x, int y, Figure[,] Board, List<int[]> list, int[] King)
+        {
+            int[] stepX = { 1, 1, 1, 0, 0, -1, -1, -1 };
+            int[] stepY = { 0, -1, 1, -1, 1, 0, -1, 1 };
+            for (int i = 0; i < 8; i++)
+            {
+
+                if (Validate.Validate_(x + stepX[i], y + stepY[i]) == true)
+                {
+
+                    if (Board[x + stepX[i], y + stepY[i]] != null)
+                    {
+                        if (Board[x + stepX[i], y + stepY[i]].getColor() != this.Color)
+                        {
+
+
+                            if (Validate.isCheck(x, y, x + stepX[i], y + stepY[i], this.Color, Board, new int[] { x + stepX[i], y + stepY[i] }) == false)
+                            {
+                                list.Add(new int[] { x, y, x + stepX[i], y + stepY[i] });
+                            }
+                        }
+                    }
+                    else
+                    {
+
+
+                        if (Validate.isCheck(x, y, x + stepX[i], y + stepY[i], this.Color, Board, new int[] { x + stepX[i], y + stepY[i] }) == false)
+                        {
+                            list.Add(new int[] { x, y, x + stepX[i], y + stepY[i] });
+                        }
+                    }
+
+                }
+            }
+            Castling(x, y, Board, list, King);
+           
+
+
+        }
+        public override bool CanReach(int x, int y, Figure[,] Board, int[] King)
+        {
+            int[] stepX = { 1, 1, 1, 0, 0, -1, -1, -1 };
+            int[] stepY = { 0, -1, 1, -1, 1, 0, -1, 1 };
+            bool check = false;
+            if(x+1==King[0] || x - 1 == King[0] || y + 1 == King[1] || y -1  == King[1])
+            {
+                check = true;
+            }
+            if (check == false)
+            {
+                return false;
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                if(x+stepX[i]==King[0] && y + stepY[i] == King[1])
+                {
+                    return true;
+                }
+            }
+
+                return false;
+        }
+        public override void ChangeFirstTurn()
+        {
+            
+            this.FirstTurn = false;
+        }
+      
+        public void Castling(int x, int y, Figure[,] Board, List<int[]> list, int[] King)
+        {
+           
+            if (this.FirstTurn == true)
+            {
+
+                if (Validate.isCheck(x, y, x, y, this.Color, Board, King) == false)
+                {
+                   
+                    if (Board[x, y + 1] == null && Board[x, y + 2] == null)
+                    {
+                        
+                        if (Board[x, 7] != null && Board[x, 7].GetType().ToString() == "chess.Rook" && Board[x, 7].getFirstTurn() == true && Board[x,7].getColor()==this.Color)
+                        {
+                            
+                            if (Validate.isCheck(x, y, x, y + 1, this.Color, Board, new int[] { x, y + 1 }) == false && Validate.isCheck(x, y, x, y + 2, this.Color, Board, new int[] { x, y + 2 }) == false)
+                            {
+                                list.Add(new int[] { x, y, x, y + 2 });
+                            }
+                        }
+                    }
+                    if (Board[x, y -1] == null && Board[x, y -2] == null && Board[x, y - 3]==null)
+                    {
+                       
+                        if (Board[x, 0] != null && Board[x, 0].GetType().ToString() == "chess.Rook" && Board[x, 0].getFirstTurn() == true && Board[x, 0].getColor() == this.Color)
+                        {
+
+                            if (Validate.isCheck(x, y, x, y - 1, this.Color, Board, new int[] { x, y - 1 }) == false && Validate.isCheck(x, y, x, y - 2, this.Color, Board, new int[] { x, y - 2 }) == false)
+                            {
+                                list.Add(new int[] { x, y, x, y - 2 });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public override Figure Copy()
+        {
+            return new King(this.Color,this.FirstTurn);
+        }
+
+
+    }
+}
---
+++ chess/Pawn.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chess
+{
+    class Pawn:Figure
+    {
+        private bool FirstTurn;
+        
+        public Pawn(Char color,bool FirstTurn)
+        {
+            this.Color = color;
+            this.FirstTurn = FirstTurn;
+        }
+        public override  void Moves(int x, int y, Figure[,] Board, List<int[]> list, int[] King)
+        {
+            int step = 0;
+            if (this.Color == 'b')
+            {
+                step = 1;
+            }
+            else if(this.Color == 'w')
+            {
+                step = -1;
+            }
+            
+            if (Validate.Validate_(x + step, y) == true)
+            {
+                if (Board[x + step, y] == null)
+                {
+                    
+                    if (Validate.isCheck(x, y, x + step, y , this.Color, Board, King) == false)
+                    {
+                        
+                        list.Add(new int[] { x, y, x + step, y });
+                    }
+                    
+                    if (this.FirstTurn == true)
+                    {
+                        if (Board[x + step*2, y] == null)
+                        {
+                            if (Validate.isCheck(x, y, x + step*2, y, this.Color, Board, King) == false)
+                            {
+                                list.Add(new int[] { x, y, x + step * 2, y });
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            if (Validate.Validate_(x + step, y + 1) == true)
+            {
+                if (Board[x + step, y+1] != null)
+                {   
+                    if(Board[x + step, y+1].getColor() != this.Color)
+                    {
+                        if (Validate.isCheck(x, y, x + step, y + 1, this.Color, Board, King) == false)
+                        {
+                            list.Add(new int[] { x, y, x + step, y + 1 });
+                        }
+                    }
+                    
+                }
+            }
+            if (Validate.Validate_(x + step, y - 1) == true)
+            {
+                if (Board[x + step, y-1] != null)
+                {
+                    if (Board[x + step, y-1].getColor() != this.Color)
+                    {
+                        if (Validate.isCheck(x, y, x + step, y - 1, this.Color, Board, King) == false)
+                        {
+                            list.Add(new int[] { x, y, x + step, y - 1 });
+                        }
+                    }
+
+                }
+            }
+            
+
+
+        }
+        public override bool CanReach(int x, int y, Figure[,] Board, int[] King)
+        {
+            int step = 0;
+            if (this.Color == 'b')
+            {
+                step = 1;
+            }
+            else if (this.Color == 'w')
+            {
+                step = -1;
+            }
+            if (x + step == King[0] && y + 1 == King[1] || x + step == King[0] && y - 1 == King[1])
+            {
+                return true;
+            }
+            return false;
+        }
+        public override void ChangeFirstTurn() { 
+            this.FirstTurn = false;
+        }
+        public override Figure Copy()
+        {
+            return new Pawn(this.Color,this.FirstTurn);
+        }
+    }
+}
